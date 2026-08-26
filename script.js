6ef3db09b4303dd18dffc4b4b5d6cf190a729ce8
Nicolas1lva <47993960+Nicolas1lva@users.noreply.github.com>
2026-08-26 20:58:30 -0300

    Add files via upload
    implementa renderização dos itens da lista

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+let listaCompras = []
+
 function adicionarItem() {
     const nome = document.getElementById('nomeItem').value;
-    const lista = document.getElementById("listaCompras");
-    const novoItem = document.createElement("li");
-    novoItem.textContent = nome;
-    lista.appendChild(novoItem);
+    const quantidade = parseInt(document.getElementById('quantidadeItem').value)
+    const valor = parseFloat(document.getElementById('valorItem').value)
+
+    const item = {
+        nome: nome,
+        quantidade: quantidade,
+        valor: valor
+    }
+    listaCompras.push(item)
+    renderizarLista();
+    atualizarValor();
+}
+function atualizarValor(){
+    let total = 0
+    for(let item of listaCompras){
+        total += item.valor*item.quantidade;
+    }
+    document.getElementById('totalCompra').textContent = `total: R$ ${total.toFixed(2)}`
+
+    console.log(total)
 }
 function menos(){
     const quantidade = document.getElementById("quantidadeItem");
@@ -18,5 +36,15 @@ function mais(){
 function add(){
     const add = document.getElementById('adcionar');
     add.classList.toggle("aberto");
-    console.log(add.classList);
+}
+function renderizarLista(){
+    const lista = document.getElementById("listaCompras")
+    lista.innerHTML = "";
+    for(let item of listaCompras){
+        const li = document.createElement('li');
+        li.textContent=`${item.nome}  |  ${item.quantidade} | ${(item.valor*item.quantidade).toFixed(2)}`
+        lista.appendChild(li)
+        console.log(li)
+
+    }
 }
